fa226ded4f7d262f3d49284da3925717e7bbbd71
almuar <amut.denia@gmail.com>
2021-05-19 21:57:47 +0200

    Obtenemos arrays interesantes, falta separar lo que queremos

--- index.js
+++ index.js
@@ -1,48 +1,23 @@
-
-    //Queremos que nuestro script se inicie cuando cargue la pagina
-    window.addEventListener("DOMContentLoaded", function (e) {
-        // Con esto vemos todo el documento de ditto
-        // AVISO:   https://pokeapi.co/api/v2/pokemon/1' = Bulbasur
-        //          https://pokeapi.co/api/v2/pokemon/2' = Ivysaur
-
-        // Cada vez que hago refresh aumenta el bucle
-        for (let contador = 1; contador < 5; contador++) {
-            let url = 'https://pokeapi.co/api/v2/pokemon/' + contador;
-            fetch(url)
-                .then(response => response.json())
-                .then(function (data) {
-                    const pokemon = document.querySelector(".pokemon");
-                    const nombrePokemon = pokemon.querySelector(".nombrePokemon");
-                    const numeroPokemon = pokemon.querySelector(".numeroPokemon");
-                    const tipoPokemon = pokemon.querySelector(".tipoPokemon");
-
-                    nombrePokemon.innerText = data.name;
-                    numeroPokemon.innerText = data.id;
-                    tipoPokemon
-                        .innerText =
-                        data.types[0].type.name;
-                });
-        }
+//Queremos SI o SI que la pagina este cargada. 
+//window.addEventListener('DOMContentLoaded', () => {
 
 
 
-        /*
-            fetch('https://pokeapi.co/api/v2/pokemon/2')
-                .then(response => response.json())
-                .then(function (data) {
-                    const pokemon = document.querySelector(".pokemon");
-                    const nombrePokemon = pokemon.querySelector(".nombrePokemon");
-                    const numeroPokemon = pokemon.querySelector(".numeroPokemon");
-                    const tipoPokemon = pokemon.querySelector(".tipoPokemon");
 
-                    nombrePokemon.innerText = data.name;
-                    numeroPokemon.innerText = data.id;
-                    tipoPokemon.innerText = data.types[0].type.name;
-                });
+const url_API = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
 
+const fetchData = async () => {
+    try { // Por si tirara error
+        const res = await fetch(url_API)
+        const data = await res.json()
+        // Obtenemos nombre de todos los pokemons
+        const arrayNombres = data.results.map(pokemon => pokemon.name)
+        console.log(arrayNombres)
+        const arrayURLs = data.results.map(pokemon => pokemon.url)
+        console.log(arrayURLs)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-
-        */
-
-
-    });
+fetchData()
